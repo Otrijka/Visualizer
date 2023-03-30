47a4bf3da0fcd948e23be3c7d526a690ab09a7c3
Otrijka <122968877+Otrijka@users.noreply.github.com>
2023-03-30 21:19:56 +0300

    сделал алгоритм + визуализировал его(нужны правки)

---
+++ aStar.js
@@ -0,0 +1,124 @@
+
+function heuristic(position0, position1) {
+    let d1 = Math.abs(position1.x - position0.x);
+    let d2 = Math.abs(position1.y - position0.y);
+    return d1 + d2;
+}
+
+function GridPoint(x, y) {
+    this.x = x; //x location of the grid point
+    this.y = y; //y location of the grid point
+    this.f = 0; //total cost function
+    this.g = 0; //cost function from start to the current grid point
+    this.h = 0; //heuristic estimated cost function from current grid point to the goal
+    this.neighbors = []; // neighbors of the current grid point
+    this.parent = undefined; // immediate source of the current grid point
+
+    // update neighbors array for a given grid point
+    this.updateNeighbors = function (grid) {
+        let i = this.x;
+        let j = this.y;
+        if (i + 1 < size && matrix[i + 1][j] != 'x') {
+            this.neighbors.push(grid[i + 1][j]);
+        }
+        if (i - 1 >= 0 && matrix[i - 1][j] != 'x') {
+            this.neighbors.push(grid[i - 1][j]);
+        }
+        if (j + 1 < size && matrix[i][j + 1] != 'x') {
+            this.neighbors.push(grid[i][j + 1]);
+        }
+        if (j - 1 >= 0 && matrix[i][j - 1] != 'x') {
+            this.neighbors.push(grid[i][j - 1]);
+        }
+    };
+}
+
+function init() {
+    for (let i = 0; i < size; i++) {
+        grid[i] = new Array(size);
+    }
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            grid[i][j] = new GridPoint(i, j);
+        }
+    }
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            grid[i][j].updateNeighbors(grid);
+        }
+    }
+}
+
+
+function findPath(startY, startX, endY, endX) {
+    console.log('start search');
+    init();
+    let startP = grid[startY][startX];
+    let endP = grid[endY][endX];
+    let current;
+    openSet.push(startP);
+
+    while (openSet.length > 0) {
+        //assumption lowest index is the first one to begin with
+        let lowestIndex = 0;
+        for (let i = 0; i < openSet.length; i++) {
+            if (openSet[i].f < openSet[lowestIndex].f) {
+                lowestIndex = i;
+            }
+        }
+        current = openSet[lowestIndex];
+
+        if (current === endP) {
+            let temp = current;
+            path.push(temp);
+            while (temp.parent) {
+                path.push(temp.parent);
+                temp = temp.parent;
+            }
+            pathIsFind = true;
+            console.log("Path has found");
+            // return the traced path
+            return path.reverse();
+        }
+
+        //remove current from openSet
+        openSet.splice(lowestIndex, 1);
+        //add current to closedSet
+        closedSet.push(current);
+
+        let neighbors = current.neighbors;
+
+        for (let i = 0; i < neighbors.length; i++) {
+            let neighbor = neighbors[i];
+
+            if (!closedSet.includes(neighbor)) {
+                let possibleG = current.g + 1;
+
+                if (!openSet.includes(neighbor)) {
+                    openSet.push(neighbor);
+                } else if (possibleG >= neighbor.g) {
+                    continue;
+                }
+
+                neighbor.g = possibleG;
+                neighbor.h = heuristic(neighbor, end);
+                neighbor.f = neighbor.g + neighbor.h;
+                neighbor.parent = current;
+            }
+        }
+    }
+
+    //no solution by default
+    pathIsFind = false;
+    console.log("No path");
+    let temp = current;
+    path.push(temp);
+    while (temp.parent) {
+        path.push(temp.parent);
+        temp = temp.parent;
+    }
+    // return the traced path
+    return path.reverse();
+}
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function makeMatrix() {
+function makeMatrix(size) {
   let matrix = [];
   for (let i = 0; i < size; i++) {
     matrix[i] = [];
@@ -13,7 +13,7 @@ function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function PaintTable(percent) {
+function PaintTable() {
   const mainPlace = document.querySelector(".mainPlace");
 
   const table = document.createElement("table");
@@ -30,14 +30,14 @@ function PaintTable(percent) {
     table.appendChild(tr);
   }
   mainPlace.appendChild(table);
-  makeMap(percent);
+  makeMap();
 }
 
 function makeGrass() {
   let cells = document.querySelector('td');
 }
 
-function makeMap(percent) {
+function makeMap() {
   let marked = [];
   let count = Number(size * size * percent / 100);
   for (let i = 0; i < count; i++) {
@@ -55,129 +55,48 @@ function makeMap(percent) {
   }
 }
 
-function heuristic(position0, position1) {
-  let d1 = Math.abs(position1.x - position0.x);
-  let d2 = Math.abs(position1.y - position0.y);
-
-  return d1 + d2;
+function pointsHere() {
+  return start != undefined && end != undefined;
 }
 
-function GridPoint(x, y) {
-  this.x = x; //x location of the grid point
-  this.y = y; //y location of the grid point
-  this.f = 0; //total cost function
-  this.g = 0; //cost function from start to the current grid point
-  this.h = 0; //heuristic estimated cost function from current grid point to the goal
-  this.neighbors = []; // neighbors of the current grid point
-  this.parent = undefined; // immediate source of the current grid point
-
-  // update neighbors array for a given grid point
-  this.updateNeighbors = function (grid) {
-    let i = this.x;
-    let j = this.y;
-    if (i < cols - 1) {
-      this.neighbors.push(grid[i + 1][j]);
-    }
-    if (i > 0) {
-      this.neighbors.push(grid[i - 1][j]);
-    }
-    if (j < rows - 1) {
-      this.neighbors.push(grid[i][j + 1]);
-    }
-    if (j > 0) {
-      this.neighbors.push(grid[i][j - 1]);
-    }
-  };
-}
-
-function init() {
-  //making a 2D array
-  for (let i = 0; i < cols; i++) {
-    grid[i] = new Array(rows);
-  }
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j] = new GridPoint(i, j);
-    }
-  }
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j].updateNeighbors(grid);
-    }
-  }
-
-  start = grid[0][0];
-  end = grid[cols - 1][rows - 1];
-
-  openSet.push(start);
-
-  console.log(grid);
-}
-
-
-
-function search() {
-  init();
-  while (openSet.length > 0) {
-    //assumption lowest index is the first one to begin with
-    let lowestIndex = 0;
-    for (let i = 0; i < openSet.length; i++) {
-      if (openSet[i].f < openSet[lowestIndex].f) {
-        lowestIndex = i;
+function drawPath(){
+  let cell = document.querySelectorAll('td');
+  let indexArr = [];
+  let draw = setInterval(function(){
+    if (path.length == 0){
+      if (pathIsFind == false){
+        indexArr.forEach(index =>{
+          cell[index].style.backgroundColor = 'red';
+        })
       }
+      clearInterval(draw);
     }
-    let current = openSet[lowestIndex];
-
-    if (current === end) {
-      let temp = current;
-      path.push(temp);
-      while (temp.parent) {
-        path.push(temp.parent);
-        temp = temp.parent;
-      }
-      console.log("DONE!");
-      // return the traced path
-      return path.reverse();
+    else{
+      let current = path[0];
+      let x = current.y;
+      let y = current.x;
+      let index = Number(y * size + x);
+      indexArr.push(index);
+      cell[index].style.backgroundColor = 'blue';
+      path.shift();
     }
-
-    //remove current from openSet
-    openSet.splice(lowestIndex, 1);
-    //add current to closedSet
-    closedSet.push(current);
-
-    let neighbors = current.neighbors;
-
-    for (let i = 0; i < neighbors.length; i++) {
-      let neighbor = neighbors[i];
-
-      if (!closedSet.includes(neighbor)) {
-        let possibleG = current.g + 1;
-
-        if (!openSet.includes(neighbor)) {
-          openSet.push(neighbor);
-        } else if (possibleG >= neighbor.g) {
-          continue;
-        }
-
-        neighbor.g = possibleG;
-        neighbor.h = heuristic(neighbor, end);
-        neighbor.f = neighbor.g + neighbor.h;
-        neighbor.parent = current;
-      }
-    }
-  }
-
-  //no solution by default
-  return [];
+  },60)
 }
+
 //Начало программы
-window.matrix;
-window.size
-let start;
-let end;
+let matrix = [];
+let size;
+let percent;
+
+let start = {};
+let end = {};
+
+let openSet = [];
+let closedSet = [];
 let path = [];
+let grid;
+
+let pathIsFind;
 
 document.querySelector('.makeTableBtn').addEventListener('click', function () {
   if (document.querySelector('.mainPlace').childElementCount != 0) document.querySelector('.mainPlace').removeChild(document.querySelector('table'));
@@ -192,9 +111,10 @@ document.querySelector('.makeTableBtn').addEventListener('click', function () {
     size = 100;
     document.querySelector('.inputSize').value = size;
   }
-  let percent = document.querySelector('.wallPercent').value;
-  matrix = makeMatrix();
-  PaintTable(percent);
+  grid = new Array(Number(size));
+  percent = document.querySelector('.wallPercent').value;
+  matrix = makeMatrix(size);
+  PaintTable();
 })
 
 document.querySelector('.makeWallsBtn').addEventListener('click', function () {
@@ -270,5 +190,16 @@ document.querySelector('.makeGrassBtn').addEventListener('click', function () {
       cells[i].style.backgroundColor = 'darkGreen';
       matrix[y][x] = 0;
     })
+  }
+})
+
+document.querySelector('.findPath').addEventListener('click',function(){
+  if (pointsHere()){
+    document.getElementsByClassName('findPath').disabled = true;
+    findPath(start.Y,start.X,end.Y,end.X);
+    drawPath();
+  }
+  else{
+    document.getElementsByClassName('findPath').disabled = true;
   }
 })
