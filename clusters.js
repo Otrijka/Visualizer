511325e0f05ed94e926a9a303205e082b65b871e
Otrijka <122968877+Otrijka@users.noreply.github.com>
2023-04-01 23:52:35 +0300

    Поправил баг с кластерами, теперь я понимаю, что JS коварный ЯП

--- clusters.js
+++ clusters.js
@@ -1,36 +1,29 @@
-function makeMatrix(size) {
-  let matrix = [];
-  for (let i = 0; i < size; i++) {
-    matrix[i] = [];
-    for (let j = 0; j < size; j++) {
-      matrix[i][j] = { y: i, x: j };
-    }
-  }
-  return matrix;
-}
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function makeStartCentriods(centroidsCounter) {
+function makeStartCentriods() {
   while (centroids.length != centroidsCounter) {
-    let temp = getRandomInRange(0, points.length - 1);
-    if (!centroids.includes(points[temp])) {
-      centroids.push(points[temp]);
+    //Эта строчка исправила все проблемы (шлубокое копировние нахрен)
+    const pointsClone = JSON.parse(JSON.stringify(points));
+    let index = getRandomInRange(0, pointsClone.length - 1);
+    if (!centroids.includes(pointsClone[index])) {
+      centroids.push(pointsClone[index]);
     }
   }
-  console.log(centroids);
+  return centroids;
 }
 
 function dist(point1, point2) {
   return Math.abs(point2.y - point1.y) + Math.abs(point2.x - point1.x);
 }
 
-function kMean(points, centroidsCounter) {
+function kMean() {
+  if (centroidsCounter == undefined) return;
   makeStartCentriods(centroidsCounter);
 
-  for (let iterator = 0; iterator < 200; iterator++) {
+  for (let iterator = 0; iterator < 100; iterator++) {
 
     let distancePointsToCentroids = new Array(points.length);
     for (let i = 0; i < points.length; i++) {
@@ -43,13 +36,7 @@ function kMean(points, centroidsCounter) {
         distancePointsToCentroids[i].push(distance);
       }
     }
-    //console.log(distancePointsToCentroids);
-
-    let tempCentroid = new Array(centroidsCounter);
-    for (let i = 0; i < centroidsCounter; i++) {
-      tempCentroid[i] = [];
-    }
-    //console.log(tempCentroid);
+    console.log(distancePointsToCentroids);
 
     let tempArr = new Array(centroidsCounter);
     for (let i = 0; i < centroidsCounter; i++) {
@@ -72,34 +59,21 @@ function kMean(points, centroidsCounter) {
       let avgX = 0, avgY = 0;
       let countEl = 0;
       for (let j = 0; j < tempArr[i].length; j++) {
-        if (tempArr[i][j] != undefined) {
-          avgX += Number(tempArr[i][j].x);
-          avgY += Number(tempArr[i][j].y);
-          countEl += 1;
-        }
+        avgX += Number(tempArr[i][j].x);
+        avgY += Number(tempArr[i][j].y);
+        countEl += 1;
+
       }
       avgX = Math.round(avgX / countEl);
       avgY = Math.round(avgY / countEl);
       centroids[i].x = avgX;
       centroids[i].y = avgY;
     }
-    if (iterator == 199) {
-      console.log("final Results:");
-      for (let i = 0; i < centroidsCounter; i++) {
-        console.log(`Cluster ${i} consists:`);
-        for (let j = 0; j < tempArr[i].length; j++) {
-          if (tempArr[i][j] != undefined) console.log(tempArr[i][j]);
-        }
-      }
       answer = tempArr;
-      console.log(-1);
-      return;
     }
   }
-  //console.log(centroids);
-}
 
-function drawClusters(answer, centroidsCounter) {
+function drawClusters() {
   for (let i = 0; i < centroidsCounter; i++) {
     for (let j = 0; j < answer[i].length; j++) {
       let y = answer[i][j].y;
@@ -111,16 +85,17 @@ function drawClusters(answer, centroidsCounter) {
     }
   }
 }
+
 const canvas = document.querySelector('.myCanvas');
 const ctx = canvas.getContext('2d');
 
-let centroidsCounter;
+let centroidsCounter = 2;
 let points = [];
 let centroids = [];
 let answer = [];
-let colors = ['red', 'green', 'blue', 'pink', 'orange', 'darkBlue', 'purple', 'brown', 'yellow', 'gray'];
+let colors = ['red', 'green', 'blue', 'pink', 'orange', 'darkBlue', 'purple', 'brown', 'gray'];
 
-canvas.addEventListener('mouseup', function (event) {
+canvas.addEventListener('mousedown', function (event) {
   const x = event.clientX - canvas.offsetLeft;
   const y = event.clientY - canvas.offsetTop;
 
@@ -138,19 +113,20 @@ findBtn.addEventListener('click', function () {
   }
   else {
     centroidsCounter = document.querySelector('.clusterCounter').value;
-    console.log("START!!!");
-    kMean(points, centroidsCounter);
-    drawClusters(answer, centroidsCounter);
-    drawClusters(answer, centroidsCounter);
+    if (centroidsCounter < 1) centroidsCounter = 1;
+    if (centroidsCounter > 9) centroidsCounter = 9;
+    kMean();
+    drawClusters(centroidsCounter);
 
     for (let i = 0; i < centroidsCounter; i++) {
       let x = centroids[i].x;
       let y = centroids[i].y;
       ctx.beginPath();
-      ctx.arc(x, y, 10, 0, 2 * Math.PI);
+      ctx.arc(x, y, 4, 0, 2 * Math.PI);
       ctx.fillStyle = 'yellow';
       ctx.fill();
-      ctx.closePath();
     }
   }
 })
+
+
